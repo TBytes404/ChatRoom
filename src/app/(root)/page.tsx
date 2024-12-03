2e53423c8c3baa04e2ade201d907e65c9434d3b7
TBytes404 <TBytes404@icloud.com>
2024-12-03 15:02:27 +0530

    prisma sqlite

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,3 +1,13 @@
-export default function Page() {
-  return <h1>Chat Room</h1>;
+import { prisma } from "@/lib/prisma";
+
+export default async function Page() {
+  const users = await prisma.user.findMany();
+  return (
+    <main>
+      <h1 className="text-3xl">Chat Room</h1>
+      <ul>
+        {users.map(({ id, name }) => <li key={id}>{name}</li>)}
+      </ul>
+    </main>
+  );
 }
